e901e76eab0b73890657e9ed38dc30b15eeb5e3d
lucianrica <lucianrica@gmail.com>
2021-04-24 22:09:18 +0100

    serve index html if bad request on heroku

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/utodocs'));
 
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/utodocs/'}, obj),
+// );
+
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/utodocs/'}, obj),
+    res.sendFile(path.join(__dirname, 'dist/utodocs/index.html') )
 );
 
 // Start the app by listening on the default Heroku port
