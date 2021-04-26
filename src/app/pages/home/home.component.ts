0edde854280ff0136fa9ecf24cb5576ff4d0ae06
lucianrica <lucianrica@gmail.com>
2021-04-26 19:11:15 +0100

    add home page improvement

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Prism from 'prismjs';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent{ }
+export class HomeComponent{ 
+    Prism = Prism
+}
