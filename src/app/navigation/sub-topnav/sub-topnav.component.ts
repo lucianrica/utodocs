b5da801a248f645b0ebf29e6fdedb5542ac2502e
lucianrica <lucianrica@gmail.com>
2021-04-29 12:16:55 +0100

    add active class to sublink

--- src/app/navigation/sub-topnav/sub-topnav.component.ts
+++ src/app/navigation/sub-topnav/sub-topnav.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
     styleUrls: ['./sub-topnav.component.css']
 })
 
-export class SubTopnavComponent { }
+export class SubTopnavComponent { 
+    setActiveClass() {
+        return "active"
+    }
+}
